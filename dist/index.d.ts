b2927f619cb3b4e6d08436d696c72fe17d7c4583
grignaffini <grignaffinipaolo@gmail.com>
2024-03-11 11:26:45 +0100

    ignore files

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-type StenoChatProps = {
-    id?: string;
-    origin: string;
-    url?: string;
-    position?: 'left' | 'right';
-};
-declare const StenoChat: ({ id, origin, url, position }: StenoChatProps) => null;
-export default StenoChat;
